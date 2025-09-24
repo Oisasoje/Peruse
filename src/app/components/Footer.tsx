7f53947ce51fa27527b794d9ad7441e127d72f67
Victor Ohikhemota <ohikhemotaoovictor@gmail.com>
2025-09-24 14:27:32 +0100

    Fixed footer: added link to Quiz

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -6,15 +6,17 @@ const Footer = () => {
   const { activePage, setActivePage } = useActivePage();
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-[#0b2f33] border-slate-600 flex justify-around items-center py-3 md:hidden z-50">
-      <button
-        onClick={() => setActivePage("take-quiz")}
-        className={`flex flex-col items-center w-25 py-2 rounded-lg ${
-          activePage === "take-quiz" ? "bg-[#14545b]" : ""
-        }`}
-      >
-        <Lightbulb size={20} color="yellow" />
-        <span className="text-xs mt-1">Quiz</span>
-      </button>
+      <Link href={"/take-quiz"}>
+        <button
+          onClick={() => setActivePage("take-quiz")}
+          className={`flex flex-col items-center w-25 py-2 rounded-lg ${
+            activePage === "take-quiz" ? "bg-[#14545b]" : ""
+          }`}
+        >
+          <Lightbulb size={20} color="yellow" />
+          <span className="text-xs mt-1">Quiz</span>
+        </button>
+      </Link>
 
       <button
         onClick={() => setActivePage("get-premium")}
